be079add9e0b6e90e96e71d73a49412708e9c2a1
Sushant0412 <mikesushant05@gmail.com>
2024-05-31 19:44:02 +0530

    added for loop to render all products

---
+++ scripts/amazon.js
@@ -0,0 +1,54 @@
+import { products } from "../data/products.js";
+
+const productContainer = document.querySelector(".products-grid");
+
+let productHTML = "";
+
+products.forEach((p, index) => {
+  productHTML += `
+      <div class="product-container">
+        <div class="product-image-container">
+          <img class="product-image" src="${p.image}" />
+        </div>
+
+        <div class="product-name limit-text-to-2-lines">
+          ${p.name}
+        </div>
+
+        <div class="product-rating-container">
+          <img class="product-rating-stars" src="images/ratings/rating-${
+            p.rating.stars * 10
+          }.png" />
+          <div class="product-rating-count link-primary">${p.rating.count}</div>
+        </div>
+
+        <div class="product-price">$${(p.priceCents / 100).toFixed(2)}</div>
+
+        <div class="product-quantity-container">
+          <select>
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+
+        <div class="product-spacer"></div>
+
+        <div class="added-to-cart">
+          <img src="images/icons/checkmark.png" />
+          Added
+        </div>
+
+        <button class="add-to-cart-button button-primary">Add to Cart</button>
+      </div>
+    `;
+});
+
+productContainer.innerHTML = productHTML;
