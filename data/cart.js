b51d03118ceb28557d1960118e2cd15808d0678b
Sushant0412 <mikesushant05@gmail.com>
2024-06-01 19:58:12 +0530

    added js to checkout and render items

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,13 @@
-export const cart = [];
+export const cart = [
+  {
+    productId: "77919bbe-0e56-475b-adde-4f24dfed3a04",
+    quantity: 2,
+  },
+  {
+    productId: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+    quantity: 4,
+  },
+];
 
 export function addToCart(productId) {
   let matchingItem;
