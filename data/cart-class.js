aa7aa8cfb9113005ce4ee5556f9f6e0e18ebcb92
Sushant0412 <mikesushant05@gmail.com>
2024-06-07 20:19:15 +0530

    added private properties

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,14 +1,14 @@
 class Cart {
   cartItems = undefined;
-  localStorageKey = undefined;
+  #localStorageKey = undefined;
 
   constructor(cartName) {
-    this.localStorageKey = cartName;
-    this.loadFromStorage();
+    this.#localStorageKey = cartName;
+    this.#loadFromStorage();
   }
 
-  loadFromStorage() {
-    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+  #loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
 
     if (!this.cartItems) {
       this.cartItems = [
@@ -27,7 +27,7 @@ class Cart {
   }
 
   saveToStorage() {
-    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
   }
 
   addToCart(productId) {
